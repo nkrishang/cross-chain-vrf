a5498898db83c9876c3ed381f7b59af6ac0550db
nkrishang <kn1457@nyu.edu>
2021-07-30 13:42:19 -0400

    Added util vars: fxPortal and chainlink vrf

---
+++ utils/maticBridge.ts
@@ -0,0 +1,11 @@
+export const fxPortalTestnets = {
+  checkPointManager: "0x2890bA17EfE978480615e330ecB65333b880928e",
+  fxRoot: "0x3d1d3E34f7fB6D26245E6640E1c50710eFFf15bA", // Goerli
+  fxChild: "0xCf73231F28B7331BBe3124B907840A94851f9f11" // Mumbai
+}
+
+export const fxPortalMainnet = {
+  checkPointManager: "0x86e4dc95c7fbdbf52e33d563bbdb00823894c287",
+  fxRoot: "0xfe5e5D361b2ad62c541bAb87C45a0B9B018389a2", // Mainnet
+  fxChild: "0x8397259c983751DAf40400790063935a11afa28a" // Matic
+}
